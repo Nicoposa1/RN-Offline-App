4a758cb1beaa4f5559e32a3053991f72a6325686
Nicolas Posa <nicoposa57@gmail.com>
2025-03-10 18:07:00 -0300

    Task container

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,13 @@
 import { SafeAreaView, StyleSheet, View } from 'react-native';
 
 import { TaskListItem } from '@/src/components/TaskListItem';
+import { TaskList } from '@/src/components/TaskList';
 
 export default function HomeScreen() {
   return (
     <SafeAreaView style={styles.container}>
       <View style={{padding: 10}}>
-        <TaskListItem task={{description: "First task"}} />
-        <TaskListItem task={{description: "Second task"}} />
-        <TaskListItem task={{description: "Third task"}} />
+        <TaskList />
       </View>
     </SafeAreaView>
   );
--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,6 +1,41 @@
-import { View } from "react-native";
-
+import { View, Text, StyleSheet, FlatList } from "react-native";
+import { TaskListItem } from "./TaskListItem";
+import { useState, useEffect } from "react";
 export const TaskList = () => {
-  return <View></View>;
+  const [tasks, setTasks] = useState<{ description: string }[]>([]);
+
+  useEffect(() => {
+    setTasks([
+      { description: "First task" },
+      { description: "Second task" },
+      { description: "Third task" },
+    ]);
+  }, []);
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Todo</Text>
+      <FlatList
+        data={tasks}
+        renderItem={({ item }) => <TaskListItem task={item} />}
+        contentContainerStyle={styles.list}
+      />
+    </View>
+  )
 };
-  
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#000',
+    padding: 10,
+    borderRadius: 5,
+  },
+  title: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginVertical: 10,
+  },
+  list: {
+    gap: 5,
+  },
+});
