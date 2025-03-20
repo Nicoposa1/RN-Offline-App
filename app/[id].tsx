a99212ad2684900552aca1e5e3c373cf0ac5b497
Nicolas Posa <nicoposa57@gmail.com>
2025-03-19 23:15:19 -0300

    delete items

--- app/[id].tsx
+++ app/[id].tsx
@@ -1,13 +1,23 @@
 import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 import { Stack, useLocalSearchParams } from 'expo-router'
+import { useObject,   } from '@realm/react'
+import Task from '@/src/models/Task'
+import { BSON } from 'realm'
 
 export default function TaskDetailScreen() {
   const { id } = useLocalSearchParams()
+  const task = useObject<Task>(Task, new BSON.ObjectId(id as string))
+
+  if(!task) {
+    return <Text>Loading...</Text>
+  } 
+
+
   return (
     <View style={styles.container}>
       <Stack.Screen options={{ title: "Task Detail" }} />
-      <Text style={styles.text}>Id: {id}</Text>
+      <Text style={styles.text}>{task.description}</Text>
     </View>
   )
 }
